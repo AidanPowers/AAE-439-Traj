ec76eae39cb86ef0e8c2e0ed902e078cd86eb005
Aidan Powers <astuff@bofh.com>
2023-11-27 10:57:32 -0500

    Consant Wind

--- SingleRun.py
+++ SingleRun.py
@@ -20,7 +20,16 @@ env.set_date(
     (2023, 11, 5, 18)
 )  # Hour given in UTC time
 
-env.set_atmospheric_model(type="Forecast", file="RAP")
+#env.set_atmospheric_model(type="Forecast", file="RAP")
+
+env.set_atmospheric_model(
+    type="custom_atmosphere",
+    pressure=None,
+    temperature=300,
+    wind_u=[(0, 10/3.281)],
+    wind_v=[(0, 0)],
+)
+
 
 print(env.info())
 
@@ -49,118 +58,119 @@ DMS_H100W_14A = SolidMotor(
 deploy_charge_time = 15
 
 
-# # Uncomment to print motor information
-# print(DMS_H100W_14A.all_info())
+# Uncomment to print motor information
+print(DMS_H100W_14A.all_info())
 
-# loc_iv = Rocket(
-#     radius=10.2/100/2,
-#     mass=1.022,
-#     inertia=(.11675, .11675, .0028950),
-#     power_off_drag=fileLoc + "//PowerOff.csv",
-#     power_on_drag=fileLoc + "//PowerOn.csv",
-#     center_of_mass_without_motor=0.76,
-#     coordinate_system_orientation="nose_to_tail",
+loc_iv = Rocket(
+    radius=10.2/100/2,
+    mass=1.022,
+    inertia=(.11675, .11675, .0028950),
+    power_off_drag=fileLoc + "//PowerOff.csv",
+    power_on_drag=fileLoc + "//PowerOn.csv",
+    center_of_mass_without_motor=0.76,
+    coordinate_system_orientation="nose_to_tail",
+)
+
+loc_iv.add_motor(DMS_H100W_14A, position=1.19)
+
+rail_buttons = loc_iv.set_rail_buttons(
+    upper_button_position=0.6418,
+    lower_button_position=1.0182,
+    angular_position=45,
+)
+
+nose_cone = loc_iv.add_nose(
+    length=0.325, kind="ogive", position=0
+)
+
+fin_set = loc_iv.add_trapezoidal_fins(
+    n=3,
+    root_chord=0.171,
+    tip_chord=0.063,
+    span=0.1080,
+    position=1.02,
+    cant_angle=0,
+    sweep_length=0.143 
+    #airfoil=(fileLoc + "/data/calisto/NACA0012-radians.csv","radians"),
+)
+
+# tail = loc_iv.add_tail(
+#     top_radius=0.0635, bottom_radius=0.0435, length=0.060, position=-1.194656
 # )
 
-# loc_iv.add_motor(DMS_H100W_14A, position=1.19)
-
-# rail_buttons = loc_iv.set_rail_buttons(
-#     upper_button_position=0.6418,
-#     lower_button_position=1.0182,
-#     angular_position=45,
-# )
-
-# nose_cone = loc_iv.add_nose(
-#     length=0.325, kind="ogive", position=0
-# )
-
-# fin_set = loc_iv.add_trapezoidal_fins(
-#     n=3,
-#     root_chord=0.171,
-#     tip_chord=0.063,
-#     span=0.1080,
-#     position=1.02,
-#     cant_angle=0,
-#     sweep_length=0.143 
-#     #airfoil=(fileLoc + "/data/calisto/NACA0012-radians.csv","radians"),
-# )
-
-# # tail = loc_iv.add_tail(
-# #     top_radius=0.0635, bottom_radius=0.0435, length=0.060, position=-1.194656
-# # )
-
-# #unused
-# def fake_trigger(p, h, y):
-#     # activate main when vz < 0 m/s and z < 800 m
-#     return True if h > 0 else False
+#unused
+def fake_trigger(p, h, y):
+    # activate main when vz < 0 m/s and z < 800 m
+    return True if h > 0 else False
 
 
-# #adds a zero drag chute to move into the parahute phase
-# main = loc_iv.add_parachute(
-#     name="false",
-#     cd_s=0,
-#     trigger="apogee",      # ejection altitude in meters
+#adds a zero drag chute to move into the parahute phase
+main = loc_iv.add_parachute(
+    name="false",
+    cd_s=0,
+    trigger="apogee",      # ejection altitude in meters
+    sampling_rate=105,
+    lag=1.5,
+    noise=(0, 8.3, 0.5),
+)
+
+# drogue = loc_iv.add_parachute(
+#     name="drogue",
+#     cd_s=1.0,
+#     trigger="apogee",  # ejection at apogee
 #     sampling_rate=105,
 #     lag=1.5,
 #     noise=(0, 8.3, 0.5),
 # )
 
-# # drogue = loc_iv.add_parachute(
-# #     name="drogue",
-# #     cd_s=1.0,
-# #     trigger="apogee",  # ejection at apogee
-# #     sampling_rate=105,
-# #     lag=1.5,
-# #     noise=(0, 8.3, 0.5),
-# # )
+#time of parahute charge (name from old)
+burnout_t = 10
 
-# #time of parahute charge (name from old)
-# burnout_t = 10
+print(loc_iv.plots.static_margin())
+print(loc_iv.all_info())
+print(loc_iv.draw())
 
-# print(loc_iv.plots.static_margin())
-# print(loc_iv.all_info())
+#first flight, result is the same if I run the entire flight, and then pick out a time using the matrix.
+flight_phase1 = Flight(
+    rocket=loc_iv, environment=env, rail_length=5.2, inclination=85, heading=0, max_time_step = .1# , max_time=burnout_t, verbose = True
+    )
 
-# #first flight, result is the same if I run the entire flight, and then pick out a time using the matrix.
-# flight_phase1 = Flight(
-#     rocket=loc_iv, environment=env, rail_length=5.2, inclination=85, heading=0, max_time_step = .1# , max_time=burnout_t, verbose = True
-#     )
+print(flight_phase1.all_info())
+print("phase 1 complete")
 
-# print(flight_phase1.all_info())
-# print("phase 1 complete")
+#creates the state matrix for the second phase
+initial_solution = [
+    burnout_t,
+    flight_phase1.x(burnout_t), flight_phase1.y(burnout_t), flight_phase1.z(burnout_t),
+    flight_phase1.vx(burnout_t), flight_phase1.vy(burnout_t), flight_phase1.vz(burnout_t),
+    flight_phase1.e0(burnout_t), flight_phase1.e1(burnout_t), flight_phase1.e2(burnout_t), flight_phase1.e3(burnout_t),
+    flight_phase1.w1(burnout_t), flight_phase1.w2(burnout_t), flight_phase1.w3(burnout_t)
+]
 
-# #creates the state matrix for the second phase
-# initial_solution = [
-#     burnout_t,
-#     flight_phase1.x(burnout_t), flight_phase1.y(burnout_t), flight_phase1.z(burnout_t),
-#     flight_phase1.vx(burnout_t), flight_phase1.vy(burnout_t), flight_phase1.vz(burnout_t),
-#     flight_phase1.e0(burnout_t), flight_phase1.e1(burnout_t), flight_phase1.e2(burnout_t), flight_phase1.e3(burnout_t),
-#     flight_phase1.w1(burnout_t), flight_phase1.w2(burnout_t), flight_phase1.w3(burnout_t)
-# ]
-
-# #trigger ASAP
-# def main_trigger(p, h, y):
-#     return True
+#trigger ASAP
+def main_trigger(p, h, y):
+    return True
 
 
-# main = loc_iv.add_parachute(
-#     name="main",
-#     cd_s=0.80,
-#     trigger=main_trigger,
-#     sampling_rate=105,
-#     lag=0.01, 
-#     noise=(0, 8.3, 0.5),
-# )
+main = loc_iv.add_parachute(
+    name="main",
+    cd_s=0.80,
+    trigger=main_trigger,
+    sampling_rate=105,
+    lag=0.01, 
+    noise=(0, 8.3, 0.5),
+)
 
-# #simulate a chute only flight
-# flight_phase2 = Flight(
-#     rocket=loc_iv, environment=env, rail_length=5.2, inclination=85, heading=0, initial_solution=initial_solution #,max_time_step = .1
-#     )
+#simulate a chute only flight
+flight_phase2 = Flight(
+    rocket=loc_iv, environment=env, rail_length=5.2, inclination=85, heading=0, initial_solution=initial_solution #,max_time_step = .1
+    )
 
-# #print(flight_phase2.info())
+#print(flight_phase2.info())
 
-# #flight_phase2.trajectory_3d.plot()
-# flight_phase2.plots.trajectory_3d()
-# #flight.prints.impact_conditions()
+#flight_phase2.trajectory_3d.plot()
+flight_phase2.plots.trajectory_3d()
+#flight.prints.impact_conditions()
 
 
 
